92d6b88df29e5c0f417f90074a6f61be55b734ee
neelap <neela_reddy@outlook.com>
2016-04-29 13:49:39 +0530

    Config Test Client

---
+++ src/main/java/com/nems/config/server/ConfigServerTest.java
@@ -0,0 +1,18 @@
+package com.nems.config.server;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by NE281900 on 4/27/2016.
+ */
+@RestController
+public class ConfigServerTest {
+    @Value("${lucky-word}") String luckyWord;
+
+    @RequestMapping("/lucky-word")
+    public String showLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
